bced65c37655e0415a0441445a814b1108e2c62a
Júlia Souza <juliasouzak4@gmail.com>
2026-03-10 09:01:30 -0300

    Iniciando estudos em laravel

---
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -0,0 +1,13 @@
+<?php  
+
+namespace App\Http\Controllers;
+
+class SeriesController{
+    public function listarSeries(){
+        $series = [
+         'Lost',
+         'Game of thrones',
+         'See'
+        ];
+    }
+}
